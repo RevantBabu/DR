cb3a7466cdb2f2b4e719246245b4386417e5e622
Revant <revanth.babu@mathisys-india.com>
2019-09-03 13:55:04 +0100

    dm3 overlap

--- core/diffusion_lib_overlap.py
+++ core/diffusion_lib_overlap.py
@@ -65,7 +65,7 @@ def thresholdMatrix(sM, topN):
 
   return result
 
-embedding = SpectralEmbedding(n_components=2, affinity="precomputed", n_neighbors=0)
+embedding = SpectralEmbedding(n_components=3, affinity="precomputed", n_neighbors=0)
 coords = embedding.fit( thresholdMatrix(1/(dM+0.1), 10) ).embedding_
 #coords = embedding.fit( thresholdMatrix(np.exp(-dM), 10) ).embedding_
 #coords = embedding.fit( 1/(dM+0.0001) ).embedding_
@@ -172,6 +172,21 @@ plt.ylabel('dimension2 ($10^{-2}$)')
 #plt.savefig('../results/dm/21/1/test_dmlib_ev1.svg', format="svg")
 plt.savefig('../results/21/1/dmev2_overlap_200.png')
 plt.savefig('../results/21/1/dmev2_overlap_200.pdf')
+
+fig = plt.figure(figsize=(9,9))
+ax = plt.subplot(111)
+ax.plot(coords[:, 2][200:300]*100, 'o-', label="Target neurons")
+ax.plot(np.nonzero(cornerIdx1[200:300])[0], np.full(np.nonzero(cornerIdx1[200:300])[0].shape, -5), 'o', label="Corner1")
+ax.plot(np.nonzero(cornerIdx2[200:300])[0], np.full(np.nonzero(cornerIdx2[200:300])[0].shape, -5), 'o', label="Corner2")
+ax.plot(np.nonzero(cornerIdx3[200:300])[0], np.full(np.nonzero(cornerIdx3[200:300])[0].shape, -5), 'o', label="Corner3")
+ax.plot(np.nonzero(cornerIdx4[200:300])[0], np.full(np.nonzero(cornerIdx4[200:300])[0].shape, -5), 'o', label="Corner4")
+plt.title('Diffusion Map Dimension3')
+plt.xlabel('time (s)')
+plt.ylabel('dimension2 ($10^{-2}$)')
+#ax.legend(loc='upper left',  shadow=True, ncol=1)#bbox_to_anchor=(0.75, 1.075),
+#plt.savefig('../results/dm/21/1/test_dmlib_ev1.svg', format="svg")
+plt.savefig('../results/21/1/dmev3_overlap_200.png')
+plt.savefig('../results/21/1/dmev3_overlap_200.pdf')
 
 fig = plt.figure(figsize=(8,8))
 ax = plt.subplot(111)
